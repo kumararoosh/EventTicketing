92f5963ad824e1183f6b9cb298f460078a48905b
kumararoosh <24703104+kumararoosh@users.noreply.github.com>
2022-04-04 14:47:38 -0700

    added a server-side code with id-based check in

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ window.onload = function() {
 
     var firstNameElem = document.getElementById('firstName')
     var lastNameElem = document.getElementById('lastName')
-    firstNameElem.innerText = "First Name: " + params.firstName
-    lastNameElem.innerText = "Last Name: " + params.lastName
+    var checkedInElem = document.getElementById('checkedIn')
+    // firstNameElem.innerText = "First Name: " + params.firstName
+    // lastNameElem.innerText = "Last Name: " + params.lastName
+
+    const url = "http://localhost:3000/users?id=" + params.id
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    // console.log(xmlHttp.responseText);
+
+    data = JSON.parse(xmlHttp.responseText)
+    console.log(data)
     
+    firstNameElem.innerText = "First Name: " + data.firstName
+    lastNameElem.innerText = "Last Name: " + data.lastName
+
+    if (data.checkedIn == "1") {
+        checkedInElem.style.color = 'red'
+        checkedInElem.innerText = "PERSON ALREADY CHECKED IN"
+
+    } else {
+        checkedInElem.style.color = 'green'
+        checkedInElem.innerText = 'Checked In'
+    }
 }
