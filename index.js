2d2f4e9fce84fb8af9134b274b32465828545817
kumararoosh <24703104+kumararoosh@users.noreply.github.com>
2022-04-04 13:11:40 -0700

    Added some better formatting

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
-
 window.onload = function() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
 
     var firstNameElem = document.getElementById('firstName')
     var lastNameElem = document.getElementById('lastName')
-    firstNameElem.innerText = params.firstName
-    lastNameElem.innerText = params.lastName
+    firstNameElem.innerText = "First Name: " + params.firstName
+    lastNameElem.innerText = "Last Name: " + params.lastName
     
 }
